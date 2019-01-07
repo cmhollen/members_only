2005e4b9b438b681f70477692f35b6b1d49abdf7
Courtney Hollenkamp <cmhollenkamp@gmail.com>
2019-01-07 14:47:57 -0600

    Sessions basics complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root "users#new"
+  root "static_pages#home"
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
